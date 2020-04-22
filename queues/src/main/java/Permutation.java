88ab5c49f919cc5c0d3e02655d2c5491b077f356
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-04-21 23:48:18 -0400

    Improve output in Permutation.

--- queues/src/main/java/Permutation.java
+++ queues/src/main/java/Permutation.java
@@ -8,10 +8,8 @@ public class Permutation {
         RandomizedQueue<String> rq = new RandomizedQueue<>();
         int n = args.length == 0 ? 1 : Integer.parseInt(args[0]);
         try {
-            String str = StdIn.readString();
-            while (str != null) {
-                rq.enqueue(str);
-                str = StdIn.readString();
+            while (!StdIn.isEmpty()) {
+                rq.enqueue(StdIn.readString());
             }
         } catch (NoSuchElementException ignored) {
         }
