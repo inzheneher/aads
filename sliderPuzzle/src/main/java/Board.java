34f3dc01cdae7c4d8769ffd9b0e0285293064fe1
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-05-19 23:39:11 -0400

    Manhattan method has been implemented. 43/100.

--- sliderPuzzle/src/main/java/Board.java
+++ sliderPuzzle/src/main/java/Board.java
@@ -20,7 +20,8 @@ public class Board {
      * Unit testing (not graded)
      */
     public static void main(String[] args) {
-        Board board1 = new Board(new int[][]{{0, 1, 3}, {4, 2, 5}, {7, 8, 6}});
+        Board board1 = new Board(new int[][]{{8, 1, 3}, {4, 0, 2}, {7, 6, 5}});
+        System.out.println(board1.manhattan());
         Board board2 = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
         System.out.println(board1.toString());
         System.out.println(board1.equals(board2));
@@ -68,7 +69,16 @@ public class Board {
      * Sum of Manhattan distances between tiles and goal
      */
     public int manhattan() {
-        return 0;
+        int dX = 0;
+        int dY = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] == 0) continue;
+                dX += Math.abs(tiles[i][j] - n * (Math.ceil(tiles[i][j] / (double) n) - 1) - (j + 1));
+                dY += (int) Math.abs(Math.ceil(tiles[i][j] / (double) n) - (i + 1));
+            }
+        }
+        return dX + dY;
     }
 
     /**
